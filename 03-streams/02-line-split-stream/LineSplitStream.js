72dbdb12effa2b6f8e0b274874f7ed1eda394ab2
Alexei Zyuzin <alexeizuzin@ya.ru>
2021-01-31 23:42:02 +0300

    solution for 03-streams 02-event-loop 01-events-order

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,30 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._accum = [];
+    this._lastAccumIndex = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this._accum.push(chunk);
+
+    const tempTexts = Buffer.concat(this._accum).toString().split(os.EOL);
+
+    while (this._lastAccumIndex < (tempTexts.length - 1)) {
+      this.push(tempTexts[this._lastAccumIndex]);
+      this._lastAccumIndex++;
+    }
+    callback();
   }
 
   _flush(callback) {
+    const tempTexts = Buffer.concat(this._accum).toString().split(os.EOL);
+
+    while (this._lastAccumIndex < tempTexts.length) {
+      this.push(tempTexts[this._lastAccumIndex]);
+      this._lastAccumIndex++;
+    }
+    callback();
   }
 }
 
