05aea5b8046da55349b1a40608dc98d1c051f166
Alexei Zyuzin <alexeizuzin@ya.ru>
2021-02-21 19:45:03 +0300

    task 5, part 1, 2 and task 6

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,25 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const waiters = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    next(await new Promise((resolve) => {
+        waiters.push({ resolve, ctx });
+    }));
 });
 
+
 router.post('/publish', async (ctx, next) => {
+    if (ctx.request.body.message) {
+        while (waiters.length) {
+            let waiter = waiters.pop();
+            waiter.ctx.body = ctx.request.body.message;
+            waiter.resolve();
+        }
+    }
+    ctx.body = '';
+    next();
 });
 
 app.use(router.routes());
